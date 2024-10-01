c94046e4c80d322aba0277cb487353229560a02b
조현아 <cyj9512@naver.com>
2024-10-01 17:46:25 +0900

    feat: TransactionHistory.vue DB연동 #37

--- src/views/asset/MyAssets.vue
+++ src/views/asset/MyAssets.vue
@@ -7,12 +7,20 @@
         <span class="amount">{{ formatNumber(transactionAmount) }}원</span>
         <button class="analyze-button">분석</button>
       </div>
+      <div class="advertisement-banner">
+        <div class="banner-content">
+          <img src="../assets/images/kbpay.png" alt="광고 배너" class="banner-image" />
+          <div class="banner-text">
+            <p class="banner-title">KB Pay</p>
+            <p class="banner-subtitle">편리한 국민 생활 파트너</p>
+          </div>
+        </div>
+      </div>
 
       <div class="account">
         <span class="account-title">계좌</span>
         <span class="account-amount">{{ formatNumber(totalAccountBalance) }}원</span>
       </div>
-
       <div class="withdraw-deposit-section">
         <div class="transactions">
           <span class="transactions-title">입출금</span>
@@ -22,8 +30,8 @@
         <div class="account-list">
           <div v-for="(account, index) in accounts" :key="index" class="account-card">
             <div class="account-info">
-              <span class="transactions-title">{{ account.bankName }} - {{ account.accountHolderName }}</span>
-              <span class="account-balance">{{ formatNumber(account.amount) }}원</span>
+              <span class="transactions-title">입출금통장 {{ index + 1 }}</span>
+              <span class="account-balance">{{ formatNumber(account.balance) }}원</span>
             </div>
             <div class="transfer-button-container">
               <router-link to="/transfer">
@@ -50,25 +58,50 @@
         </div>
       </div>
 
+      <hr>
+
+      <div class="credit-card-section">
+        <span class="account-title">카드</span>
+      </div>
+
+      <div>
+        <div class="transactions">
+          <span class="transactions-title">신용카드</span>
+        </div>
+
+        <div class="account-list">
+          <div v-for="(card, index) in creditCards" :key="index" class="account-card">
+            <!-- <img
+            src="../assets/images/KB카드알파원.png" alt=" 배너" class="banner-image" /> -->
+
+            <div class="account-info">
+              <span class="transactions-title">{{ card.name }}</span>
+              <span class="account-balance credit-card-balance">{{ formatNumber(card.balance) }}원</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
       <div class="loan-section">
         <span class="account-title">대출</span>
         <div class="account-list">
           <div class="account-card">
             <div class="account-info">
               <span class="transactions-title">주택담보대출</span>
-              <span class="account-balance credit-card-balance">{{ formatNumber(loanAmount) }}원</span>
+              <span class="account-balance credit-card-balance">{{ `${formatNumber(loanAmount)}` }}원</span>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
+    </div>
     <FooterNav :buttonType="'pay'" :buttonAction="goToGroupPayPage" />
   </div>
 </template>
 
 <script>
-import apiClient from '../../api/axios.js';
 import FooterNav from '../../components/FooterNav.vue';
 
 export default {
@@ -78,54 +111,42 @@ export default {
   },
   data() {
     return {
-      userName: '',
-      accounts: [],
+      userName: '조현아',
+      accounts: [
+        { balance: 1960112 },
+        { balance: 5800 },
+        { balance: 82400 },
+      ],
       savingsAccount: {
-        balance: 0,
+        balance: 6000000,
       },
-      loanAmount: 480000000, // 대출 금액
+      creditCards: [
+        { name: 'KB알파원', balance: 150000 },
+        { name: 'KB청춘대로 톡톡', balance: 100000000 },
+        { name: '현대 네이버페이', balance: 1385290000 },
+      ],
+      loanAmount: 480000000,
     };
   },
 
   computed: {
     totalAccountBalance() {
-      return this.accounts.reduce((total, account) => total + account.amount, 0) + this.savingsAccount.balance;
+      return this.accounts.reduce((total, account) => total + account.balance, 0) + this.savingsAccount.balance;
     },
     transactionAmount() {
       return this.totalAccountBalance;
     },
     totalWithdrawDeposit() {
-      return this.accounts.reduce((total, account) => total + account.amount, 0);
+      return this.accounts.reduce((total, account) => total + account.balance, 0);
     }
   },
-
   methods: {
     formatNumber(num) {
       return num.toLocaleString();
     },
-
-async fetchAccounts() {
-  try {
-    const response = await apiClient.get('/account/all');
-    if (response.data.isSuccess) {
-      this.accounts = response.data.result.accountList;
-      this.userName = this.accounts[0].accountHolderName; // 사용자 이름 설정
-    } else {
-      console.error('계좌 정보를 가져오지 못했습니다.');
-    }
-  } catch (error) {
-    console.error('API 호출 중 오류 발생:', error);
-  }
-},
   },
-
-  mounted() {
-    this.fetchAccounts();
-  }
-};
+}
 </script>
-
-
 
 <style scoped>
 .main-container {
