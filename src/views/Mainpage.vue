15d80d2ba3c543064747cf110056289752978d70
삼성이온2\sseo2 <sseo2732@gmail.com>
2024-10-07 17:46:21 +0900

    feat: style.css 폰트-프리텐다드 추가

--- src/views/Mainpage.vue
+++ src/views/Mainpage.vue
@@ -189,18 +189,6 @@ export default {
 </script>
 
 <style scoped>
-@font-face {
-  font-family: "Pretendard-Regular";
-  src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
-    format("woff");
-  font-weight: 400;
-  font-style: normal;
-}
-
-* {
-  font-family: "Pretendard", sans-serif;
-}
-
 .main-container {
   position: relative;
 }
