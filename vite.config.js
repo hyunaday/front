0e46d70f49d54e649eae82a3777bd1c931c3f4f6
heejinKong <gmlwls3379@naver.com>
2024-10-15 05:27:59 +0900

    fix : PWA

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,63 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa'; // PWA 플러그인 임포트
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      srcDir: 'public',
+      filename: 'sw.js',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      manifest: {
+        name: '모두의결제',
+        short_name: '모두의결제',
+        theme_color: '#4DBA87',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/modu-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/modu-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/screenshots/screenshot1.png', // 경로에 맞게 수정
+            sizes: '1280x800', // 적절한 사이즈 설정
+            type: 'image/png',
+            form_factor: 'wide', // 또는 'square'와 같은 다른 값
+          },
+          {
+            src: '/screenshots/screenshot2.png', // 두 번째 스크린샷 추가
+            sizes: '1280x800',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
-      // 프록시는 로컬 개발 환경에서만 작동합니다.
       '/api': {
-        target: 'https://together-pay.store', // 실제 API 서버 URL
+        target: 'http://34.64.141.174:8080', // API 서버 주소
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    open: {
-      app: {
-        name: "Google Chrome", // 원하는 브라우저 이름
-      },
-    },
+    host: true,
+    port: 5173,
   },
 });
