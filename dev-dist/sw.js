0a566692d98145018fd92659144ef83cee37d911
삼성이온2\sseo2 <sseo2732@gmail.com>
2024-10-16 02:17:46 +0900

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g1vcmet8hk"
+    "revision": "0.gd4mi8drto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
