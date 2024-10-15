cb4789cbb0b17798ecd241ae864f42d1f077b174
삼성이온2\sseo2 <sseo2732@gmail.com>
2024-10-15 22:33:35 +0900

    feat: 로딩 창 제작

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bldmud426r8"
+    "revision": "0.g1vcmet8hk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
